9dd481be602b968ed26669ae890b7adb73829fb4
Diego Victor Boareto Pinho <48157241+DiegoVBPinho@users.noreply.github.com>
2025-03-13 15:17:41 -0300

    Novos programas

--- Moodle/Matricula.cpp
+++ Moodle/Matricula.cpp
@@ -16,18 +16,19 @@ int main()
     int N = 0;
     cin >> N;        // ler o valor da entrada do N
     Aluno alunos[N]; // N Alunos dentro do Vetor de Alunos.
+    int armazena_i = 0;
 
     // ler os dados dos alunos dentro do vetor.
 
     for (int i = 0; i < N; i++)
     {
-        cin >> alunos[i].nome;
-        cin >> alunos[i].curso;
+        getline(cin >> ws, alunos[i].nome);
+        getline(cin >> ws, alunos[i].curso);
         cin >> alunos[i].matricula;
     }
 
     string nome_pesquisa;
-    cin >> nome_pesquisa;
+    getline(cin >> ws, nome_pesquisa);
 
     bool achou = false;
     for (int i = 0; i < N && achou == false; i++)
@@ -35,7 +36,20 @@ int main()
         if (alunos[i].nome == nome_pesquisa)
         {
             achou = true;
+            armazena_i = i;
         }
     }
+
+    if (achou == false)
+    {
+        cout << "Aluno nao localizado";
+    }
+    else
+    {
+        cout << alunos[armazena_i].nome << endl;
+        cout << alunos[armazena_i].curso << endl;
+        cout << alunos[armazena_i].matricula << endl;
+    }
+
     return 0;
 }
